9f1aa72dd1401a155588e288ee13c90fab44a958
nightfish <nightfish.net@gmail.com>
2020-03-04 03:22:40 +1300

    Add null argument checks.

--- src/common/Stop.java
+++ src/common/Stop.java
@@ -18,7 +18,23 @@ public class Stop {
     private final List<Connection> connections = new ArrayList<>();
     private boolean lockConnections; // Prevents adding new connections once the object has been created.
 
-    public Stop(String id, String name, Location location) {
+    /**
+     * Creates new stop object.
+     * @param id Unique ID of the stop.
+     * @param name Name of the stop.
+     * @param location Location of the stop.
+     * @throws IllegalArgumentException When non nullable arguments are null.
+     */
+    public Stop(String id, String name, Location location) throws IllegalArgumentException {
+        if(id == null)
+            throw new IllegalArgumentException("ID must not be null.");
+
+        if(name == null)
+            throw new IllegalArgumentException("Name must not be null.");
+
+        if(location == null)
+            throw new IllegalArgumentException("Location must not be null.");
+
         this.id = id;
         this.name = name;
         this.location = location;
@@ -45,8 +61,15 @@ public class Stop {
      * @param stop Stop to connect to.
      * @param trip The trip that contains the connection.
      * @throws IllegalAccessError When method is called after the connections have been locked.
+     * @throws IllegalArgumentException When non nullable arguments are null.
      */
-    public void makeConnection(Stop stop, Trip trip) throws IllegalAccessError {
+    public void makeConnection(Stop stop, Trip trip) throws IllegalAccessError, IllegalArgumentException {
+        if(stop == null)
+            throw new IllegalArgumentException("Stop must not be null.");
+
+        if(trip == null)
+            throw new IllegalArgumentException("Trip must not be null.");
+
         if(lockConnections)
             throw new IllegalAccessError("Connections cannot be added on a locked object.");
         connections.add(new Connection(stop, trip));
