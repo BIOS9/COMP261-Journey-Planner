040eb13290cfee5860019c7dcb829b59c72c8259
nightfish <nightfish.net@gmail.com>
2020-03-04 02:35:09 +1300

    Add different constructors for constructing root, branch and leaf nodes in the trie.

--- src/search/StopNode.java
+++ src/search/StopNode.java
@@ -2,6 +2,9 @@ package search;
 
 import common.Stop;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A node in a trie structure that contains a character in the name of a
  * bus stop and a Stop object if the name is complete.
@@ -9,10 +12,38 @@ import common.Stop;
  * @author Matthew Corfiatis
  */
 class StopNode {
+    private final boolean isRoot;
     private final char nameCharacter;
     private final Stop stop;
+    private final Map<Character, StopNode> children = new HashMap<>();
+    private boolean childrenLocked;
 
+    /**
+     * Constructor for constructing a root node.
+     */
+    public StopNode() {
+        isRoot = true;
+        nameCharacter = 0;
+        stop = null;
+    }
+
+    /**
+     * Constructor for constructing a branch node.
+     * @param nameCharacter Character in the bus stop name.
+     */
+    public StopNode(char nameCharacter) {
+        isRoot = false;
+        this.nameCharacter = nameCharacter;
+        this.stop = null;
+    }
+
+    /**
+     * Constructor for constructing a leaf node.
+     * @param nameCharacter Character in the bus stop name.
+     * @param stop Stop to store.
+     */
     public StopNode(char nameCharacter, Stop stop) {
+        isRoot = false;
         this.nameCharacter = nameCharacter;
         this.stop = stop;
     }
