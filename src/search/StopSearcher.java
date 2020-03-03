95121a5e7e9cceb9174c8e45da4963ee7f01740a
nightfish <nightfish.net@gmail.com>
2020-03-04 02:26:33 +1300

    Add template methods for constructor and search by name prefix.

--- src/search/StopSearcher.java
+++ src/search/StopSearcher.java
@@ -1,5 +1,9 @@
 package search;
 
+import common.Stop;
+
+import java.util.Collection;
+
 /**
  * Manages indexing and searching for bus stops in the journey.
  *
@@ -7,4 +11,27 @@ package search;
  */
 public class StopSearcher {
 
+    /**
+     * Creates a StopSearcher object and indexes the provided stops to
+     * allow easy searching.
+     * @param stops Stops to be indexed.
+     */
+    public StopSearcher(Collection<Stop> stops) {
+        // TODO: Create trie structure for index.
+    }
+
+    /**
+     * Searches for stops by name.
+     *
+     * If one stop name exactly matches the provided value, only that
+     * one stop will be returned.
+     * If multiple stops match, multiple stops will be returned.
+     *
+     * @param name Name prefix to search for.
+     * @return A collection of stops matching the name prefix.
+     */
+    public Collection<Stop> searchName(String name) {
+        // TODO: Return the collection of stops.
+        return null;
+    }
 }
