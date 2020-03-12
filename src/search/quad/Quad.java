3a820b079def33f7387f6be915306a7868220c75
Matthew Corfiatis <matt.corf@gmail.com>
2020-03-12 22:13:57 +1300

    Add quad and quad testing

---
+++ src/search/quad/Quad.java
@@ -0,0 +1,78 @@
+package search.quad;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Represents a shape with 4 sides that can contain other quads or points.
+ * @author Matthew Corfiatis
+ */
+public class Quad {
+    public final Rectangle2D bounds;
+
+    public Quad(Rectangle2D bounds) {
+        this.bounds = bounds;
+    }
+
+    public Quad(double x, double y, double width, double height) {
+        this.bounds = new Rectangle2D.Double(x, y, width, height);
+    }
+
+    public Rectangle2D getBounds() {
+        return bounds;
+    }
+
+    /**
+     * Finds the closest point on/in the quad to a given point.
+     * @param point Point to find the closest point to.
+     * @return Closest point on/in the quad.
+     */
+    public Point2D closestPointOnQuad(Point2D point) {
+        // Find which region point is in
+        // There are 8 regions around the quad.
+        // 7 \    0    /  1
+        // ___\_______/____
+        // 6   |     |    2
+        // ____|_____|_____
+        //    /       \
+        // 5 /    4    \  3
+        // Closest point in the corner regions, is the corner of quad in that region.
+        // Closest point in any other external region is a straight line which intersects the nearest edge at a right angle.
+
+        if(point.getX() <= bounds.getMinX()) { // Point is left of rect
+            if(point.getY() <= bounds.getMinY()) { // Point is in top-left region (7)
+                return new Point2D.Double(bounds.getMinX(), bounds.getMinY()); // Return top-left corner
+            } else if(point.getY() >= bounds.getMaxY()) { // Point is in bottom-left region (5)
+                return new Point2D.Double(bounds.getMinX(), bounds.getMaxY()); // Return bottom-left corner
+            } else { // Point is in middle-left region (6)
+                return new Point2D.Double(bounds.getMinX(), point.getY()); // Return point on left edge
+            }
+        } else if(point.getX() >= bounds.getMaxX()) { // Point is right of rect
+            if(point.getY() <= bounds.getMinY()) { // Point is in top-right region (1)
+                return new Point2D.Double(bounds.getMaxX(), bounds.getMinY()); // Return top-right corner
+            } else if(point.getY() >= bounds.getMaxY()) { // Point is in bottom-right region (3)
+                return new Point2D.Double(bounds.getMaxX(), bounds.getMaxY()); // Return bottom-right corner
+            } else { // Point is in middle-right region (2)
+                return new Point2D.Double(bounds.getMaxX(), point.getY()); // Return point on right edge
+            }
+        } else { // Point is above or below rect
+            if(point.getY() <= bounds.getMinY()) { // Point is in top-middle region (0)
+                return new Point2D.Double(point.getX(), bounds.getMinY()); // Return point on top edge
+            } else if(point.getY() >= bounds.getMaxY()) { // Point is in bottom-middle region (4)
+                return new Point2D.Double(point.getX(), bounds.getMaxY()); // Return point on bottom edge
+            } else { // Point is the quad
+                // If the point is inside the rect, then return it as closest.
+                return point;
+            }
+        }
+    }
+
+    /**
+     * Finds the distance from a given point to the closest point on/in the quad.
+     * @param point Point to find the distance from.
+     * @return A distance double.
+     */
+    public double distanceToQuad(Point2D point) {
+        return point.distance(closestPointOnQuad(point));
+    }
+}
---
+++ test/quad/quadTests.java
@@ -0,0 +1,129 @@
+package quad;
+
+import org.junit.jupiter.api.Test;
+import search.quad.Quad;
+
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class quadTests {
+
+    @Test
+    public void testCreateQuad() {
+        Quad quad = new Quad(100, 150, 200, 250);
+        Rectangle2D bounds = quad.getBounds();
+
+        assertEquals(100, bounds.getMinX());
+        assertEquals(150, bounds.getMinY());
+        assertEquals(200, bounds.getWidth());
+        assertEquals(250, bounds.getHeight());
+        assertEquals(300, bounds.getMaxX());
+        assertEquals(400, bounds.getMaxY());
+    }
+
+    @Test
+    public void testClosestPointExternal() {
+        Quad quad = new Quad(100, 150, 200, 250);
+        Rectangle2D bounds = quad.getBounds();
+
+        // Corners
+
+        // top-left
+        assertEquals(new Point2D.Double(bounds.getMinX(), bounds.getMinY()),
+                quad.closestPointOnQuad(new Point2D.Double(50, 25)));
+
+        // top-right
+        assertEquals(new Point2D.Double(bounds.getMaxX(), bounds.getMinY()),
+                quad.closestPointOnQuad(new Point2D.Double(450, 25)));
+
+        // bottom-right
+        assertEquals(new Point2D.Double(bounds.getMaxX(), bounds.getMaxY()),
+                quad.closestPointOnQuad(new Point2D.Double(500, 500)));
+
+        // bottom-left
+        assertEquals(new Point2D.Double(bounds.getMinX(), bounds.getMaxY()),
+                quad.closestPointOnQuad(new Point2D.Double(50, 500)));
+
+
+        // Sides
+
+        // top
+        assertEquals(new Point2D.Double(125, bounds.getMinY()),
+                quad.closestPointOnQuad(new Point2D.Double(125, 25)));
+
+        // right
+        assertEquals(new Point2D.Double(bounds.getMaxX(), 175),
+                quad.closestPointOnQuad(new Point2D.Double(450, 175)));
+
+        // bottom
+        assertEquals(new Point2D.Double(150, bounds.getMaxY()),
+                quad.closestPointOnQuad(new Point2D.Double(150, 500)));
+
+        // left
+        assertEquals(new Point2D.Double(bounds.getMinX(), 200),
+                quad.closestPointOnQuad(new Point2D.Double(50, 200)));
+    }
+
+    @Test
+    public void testClosestPointInteral() {
+        Quad quad = new Quad(100, 150, 200, 250);
+        Rectangle2D bounds = quad.getBounds();
+
+        Point2D testPoint = new Point2D.Double(173, 300);
+
+        assertEquals(testPoint, quad.closestPointOnQuad(testPoint));
+    }
+
+    @Test
+    public void testClosestPointOnEdge() {
+        Quad quad = new Quad(100, 150, 200, 250);
+        Rectangle2D bounds = quad.getBounds();
+
+        // Left edge
+        assertEquals(new Point2D.Double(bounds.getMinX(), bounds.getMinY() + 20),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMinX(), bounds.getMinY() + 20)));
+
+        // Right edge
+        assertEquals(new Point2D.Double(bounds.getMaxX(), bounds.getMinY() + 20),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMaxX(), bounds.getMinY() + 20)));
+
+        // Top edge
+        assertEquals(new Point2D.Double(bounds.getMinX() + 20, bounds.getMinY()),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMinX() + 20, bounds.getMinY())));
+
+        // Bottom edge
+        assertEquals(new Point2D.Double(bounds.getMinX() + 20, bounds.getMaxY()),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMinX() + 20, bounds.getMaxY())));
+    }
+
+    @Test
+    public void testClosestPointOnCorner() {
+        Quad quad = new Quad(100, 150, 200, 250);
+        Rectangle2D bounds = quad.getBounds();
+
+        // top-left
+        assertEquals(new Point2D.Double(bounds.getMinX(), bounds.getMinY()),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMinX(), bounds.getMinY())));
+
+        // top-right
+        assertEquals(new Point2D.Double(bounds.getMaxX(), bounds.getMinY()),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMaxX(), bounds.getMinY())));
+
+        // bottom-right
+        assertEquals(new Point2D.Double(bounds.getMaxX(), bounds.getMaxY()),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMaxX(), bounds.getMaxY())));
+
+        // bottom-left
+        assertEquals(new Point2D.Double(bounds.getMinX(), bounds.getMaxY()),
+                quad.closestPointOnQuad(new Point2D.Double(bounds.getMinX(), bounds.getMaxY())));
+    }
+
+    @Test
+    public void testDistanceToQuad() {
+        Quad quad = new Quad(100, 150, 200, 250);
+
+        assertEquals(50, quad.distanceToQuad(new Point2D.Double(50, 226)));
+    }
+}
